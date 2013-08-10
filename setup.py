e8d4141684118dd6444467c9a3703f7b0ad5050e
Bit Shift <bitshift@bigmacintosh.net>
2013-08-10 15:53:51 +0100

    Begin work on 0.2.0.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name="Tigger",
-        version="0.1.0",
+        version="0.2.0-dev",
         packages=["tigger",],
         license="MIT",
         description="Command-line tagging tool.",
